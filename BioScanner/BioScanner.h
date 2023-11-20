2b0399c3810e1faf49ff34b47b7888104ce2bfa7
m01n008 <iamoin008@gmail.com>
2023-11-20 15:35:04 +0500

    Initial Commit

---
+++ BioScanner/BioScanner.h
@@ -0,0 +1,18 @@
+//
+//  BioScanner.h
+//  BioScanner
+//
+//  Created by Muhammad Moin Raza Khan on 20/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BioScanner.
+FOUNDATION_EXPORT double BioScannerVersionNumber;
+
+//! Project version string for BioScanner.
+FOUNDATION_EXPORT const unsigned char BioScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BioScanner/PublicHeader.h>
+
+
